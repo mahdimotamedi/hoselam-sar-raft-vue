a752ac05bee0c4aab70a3b18931c67c49351b490
mahdi motamedi <mahdi.motamedi76@gmail.com>
2018-09-27 02:52:57 +0330

    add fun fact page

--- src/views/fun-pages/FunFact.vue
+++ src/views/fun-pages/FunFact.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="fun-fact container">
-    Fun Fact !! :))))
+  <div class="fun-page fun-fact container">
+    <div class="col-md-12 page-head">
+            <span class="page-icon">
+              <img src="./../../assets/images/leaf.png">
+            </span>
+      <span class="page-title">یک مطلب در مورد جهان هستی بگو که برگام بریزه!</span>
+
+      <router-link class="back-btn mr-auto" to="/help">
+        <img src="./../../assets/images/back-icon.png" height="15">
+        بازگشت
+      </router-link>
+    </div>
+
+    <div class="col-mdf-12 content-box">
+      <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جام.</p>
+    </div>
   </div>
 </template>
 
