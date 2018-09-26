bc42f7e447285168c1f4dab91142700ba3a5247c
mahdi motamedi <mahdi.motamedi76@gmail.com>
2018-09-27 00:23:49 +0330

    add animation to homepage

---
+++ src/views/FunList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="fun-list container">
+      <div class="row">
+        <div class="col-md-12">لیست فان</div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FunList',
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,9 @@
       <div class="row">
         <div class="col-md-6 text-center">
           <h1>حوصلم پوکید !!</h1>
-          <a href="#" class="btn btn-outline-success">کمک</a>
+            <router-link to="/help" class="btn btn-outline-success">کمک</router-link>
         </div>
-        <div class="col-md-6 text-center">
+        <div class="col-md-6 text-center d-none d-md-block">
           <img src="../assets/images/homepage-logo.png" class="homepage-image">
         </div>
       </div>
@@ -13,13 +13,8 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import FooterPart from "../components/FooterPart";
 
 export default {
   name: 'home',
-  components: {
-      FooterPart,
-  }
 }
 </script>
