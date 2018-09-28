bb731924faf09ec1ce0ca63dec3eb5701223f2a0
mahdi motamedi <mahdi.motamedi76@gmail.com>
2018-09-28 18:32:03 +0330

    add marry with women data

--- src/views/fun-pages/MarryWithWoman.vue
+++ src/views/fun-pages/MarryWithWoman.vue
@@ -13,16 +13,36 @@
     </div>
 
     <content-box
-        image="women/1.jpg"
-        content="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جام." />
+        :image="image"
+        :content="content" />
   </div>
 </template>
 
 <script>
 
   import ContentBox from "../../components/ContentBox";
+  import contents from "../../data/women"
+  import randomContent from "../../common/randomContent";
+
   export default {
     name: 'MarryWithWoman',
     components: {ContentBox},
+
+
+    data()
+    {
+      return {
+        content: '',
+        image: '',
+      }
+    },
+
+    mounted()
+    {
+      randomContent.setContents(contents, 'women');
+      let content = randomContent.getRandomContent();
+      this.content = content.content;
+      this.image = content.image;
+    },
   }
 </script>
